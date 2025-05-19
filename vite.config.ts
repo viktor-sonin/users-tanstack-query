c877b09ee189f19722b01b81ea37ac6d045ac45e
Viktor Sonin <soninviktorforjob@gmail.com>
2025-05-19 19:19:55 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/skillsrock/users-tanstack-query/",
-	build: { outDir: "build/users-tanstack-query" },
+	base: "/users-tanstack-query/",
+	build: { outDir: "build" },
 	plugins: [react()],
 	resolve: {
 		alias: {
